d9630ac9cfe42927576d87f61791e128a86c3054
Sirius Cybernetics Corporation <SiriusCybernetics@bigpond.com>
2016-05-24 08:25:10 +0800

    Add project files.

---
+++ Thin RTSP Client/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thin_RTSP_Client
+{
+    public class Class1
+    {
+    }
+}
